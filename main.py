d787e5c375822cb0846f88ba67cd9a54f0954527
fastenough69 <126094565+fastenough69@users.noreply.github.com>
2024-10-23 03:31:26 +0300

    Add files via upload
    0.0 ver

---
+++ main.py
@@ -0,0 +1,60 @@
+import requests
+from string import digits
+from time import time
+import asyncio
+import aiohttp
+
+def get_time(func):
+    def decorator(*args):
+        timer = time()
+        func(*args)
+        print(time() - timer)
+    return decorator
+
+async def response_mail(sesion, mail_name: str):
+    url = 'https://api.products.aspose.app/email/api/Checker/Check'
+    headers = {
+        'Accept': '*/*',
+        'Accept-Encoding': 'gzip, deflate, br, zstd',
+        'Accept-Language': 'ru,en;q=0.9',
+        'Connection': 'keep-alive',
+        'Host': 'api.products.aspose.app',
+        'Origin': 'https://products.aspose.app',
+        'Referer': 'https://products.aspose.app/',
+        'Sec-Ch-Ua': '"Google Chrome";v="129", "Not=A?Brand";v="8", "Chromium";v="129"',
+        'Sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Windows"',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-site',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 YaBrowser/24.7.0.0 Safari/537.36',
+    }
+    data = {
+    'email': mail_name
+    }
+    async with sesion.post(url, headers=headers, data=data) as response:
+        data = await response.json()
+        return data['email'], data['isDisposable']
+
+def check_name_mail(data: list):
+    for name in data:
+        if '_' in name or ' ' in name or name[0] in digits:
+            data.remove(name)
+    return data
+
+async def main():
+    name_file = input('Введите полный путь до файла:')
+    with open(name_file, 'r') as input_file:
+        temp = input_file.readlines()
+    async with aiohttp.ClientSession() as session:
+        data = [line.split('|')[1].strip() for line in temp if line != '\n']
+        data = check_name_mail(data)
+        tasks = [response_mail(session, name + '@gmail.com') for name in data]
+        res = await asyncio.gather(*tasks)
+        with open('C:\\Users\\lisen\\OneDrive\\Рабочий стол\\output.txt', '+a') as output_file:
+            for mail, status in res:
+                output_file.writelines(f'{mail}\tstatus: {status}\n')
+
+if __name__ == '__main__':
+    asyncio.run(main())
+
